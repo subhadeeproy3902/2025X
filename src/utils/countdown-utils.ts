68b1d2183e4718e7580a4d65366d2e29c39b88a2
Subhadeep Roy <subha9.5roy350@gmail.com>
2025-01-01 00:03:02 +0530

    Update countdown-utils.ts

--- src/utils/countdown-utils.ts
+++ src/utils/countdown-utils.ts
@@ -1,5 +1,5 @@
 export const calculateTimeToEvent = () => {
-  const eventDate = new Date("2025-12-31T18:30:00-00:00");
+  const eventDate = new Date("2026-01-01T00:00:00-00:00");
   const currentDate = new Date();
   const timeRemaining = eventDate.getTime() - currentDate.getTime();
 
